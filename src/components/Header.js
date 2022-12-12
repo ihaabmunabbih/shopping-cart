1f7ad04a3ad7291c1522e55b3cb1624166c74b0f
Ihaab Munabbih <ihaab.nutech@gmail.com>
2022-12-12 08:56:51 +0700

    init

---
+++ src/components/Header.js
@@ -0,0 +1,95 @@
+import  { 
+            Navbar,
+            Container,
+            FormControl,
+            Nav,
+            Dropdown,
+            Badge,
+            Button
+        } from 'react-bootstrap';
+import { FaShoppingCart } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { CartState } from '../context/Context';
+
+const Header = () => {
+
+  const {
+    state: {cart},
+    dispatch,
+    productDispatch,
+  } = CartState();
+
+  return (
+    <Navbar bg="dark" variant="dark" style={{height: 80}} className="nav">
+        <Container>
+            <Navbar.Brand>
+                <Link to="/">Shopping Cart</Link>
+            </Navbar.Brand>
+            <Navbar.Text className="search">
+                <FormControl
+                    style={{width: 500}}
+                    placeholder="Search a product"
+                    className="m-auto"
+                    onChange={(e) => 
+                        productDispatch({
+                            type: "FILTER_BY_SEARCH",
+                            payload: e.target.value
+                        })
+                    }
+                />
+            </Navbar.Text>
+            <Nav>
+                <Dropdown>
+                    <Dropdown.Toggle variant="success">
+                        <FaShoppingCart color="white" fontSize="25px"/>
+                        <Badge bg="none">{cart.length}</Badge>
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu style={{minWidth: 370}} className="cartDropDown" >
+                        {
+                            cart.length > 0 ? (
+                                <>
+                                    {
+                                        cart.map((prod) => (
+                                            <span className="cartItem" key={prod.id}>
+                                                <img
+                                                    src={prod.image}
+                                                    className="cartItemImg"
+                                                    alt={prod.name}
+                                                />
+                                                <div className="cartItemDetail">
+                                                    <span>{prod.name}</span>
+                                                    <span>$ {prod.price.split(".")[0]}</span>
+                                                </div>
+                                                <AiFillDelete
+                                                    fontSize="20px"
+                                                    style={{cursor: "pointer"}}
+                                                    onClick={() => 
+                                                        dispatch({
+                                                            type: "REMOVE_FROM_CART",
+                                                            payload: prod,
+                                                        })}
+                                                />
+                                            </span>
+                                        ))
+                                    }
+                                    <Link to="/cart">
+                                        <Button style={{width: "95%", margin: "0 10px"}}>
+                                            Go To Cart
+                                        </Button>
+                                    </Link>
+                                </>
+                            ) : (
+                                <span style={{padding: 10}}>Cart is Empty!</span>
+                            )
+                        }
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Nav>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default Header
---
+++ src/components/Home.js
@@ -0,0 +1,54 @@
+import { CartState } from "../context/Context";
+import Filters from "./Filters";
+import SingleProduct from "./SingleProduct";
+import "./styles.css";
+
+const Home = () => {
+  const {
+    state: {products},
+    productState: {byStock, byFastDelivery, sort, byRating, searchQuery},
+  } = CartState()
+  
+  const transformProduct = () => {
+    let sortedProducts = products;
+
+    if (sort) {
+      sortedProducts = sortedProducts.sort((a,b) => (
+        sort === "lowToHigh" ? a.price - b.price : b.price - a.price
+      )) 
+    }
+
+    if (!byStock) {
+      sortedProducts = sortedProducts.filter((prod) => prod.inStock)
+    }
+
+    if (byFastDelivery) {
+      sortedProducts = sortedProducts.filter((prod) => prod.fastDelivery)
+    }
+
+    if (byRating) {
+      sortedProducts = sortedProducts.filter((prod) => prod.ratings >= byRating)
+    }
+
+    if (searchQuery) {
+      sortedProducts = sortedProducts.filter((prod) => prod.name.toLowerCase().includes(searchQuery.toLowerCase()))
+    }
+
+    return sortedProducts;
+  };
+
+  return (
+    <div className="home">
+        <Filters />
+        <div className="productContainer">
+          {
+            transformProduct().map((prod) => {
+              return <SingleProduct prod={prod} key={prod.id}/>
+            })
+          }
+        </div>
+    </div>
+  )
+}
+
+export default Home
